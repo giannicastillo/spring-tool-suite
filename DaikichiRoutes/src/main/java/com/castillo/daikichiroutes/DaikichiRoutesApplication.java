018fa8375f5bf2742221b97bca47c7096bd5c2d7
giannicastillo <66094112+giannicastillo@users.noreply.github.com>
2022-02-11 14:42:05 -0500

    Spring tool intro Daikichi routes complete!

---
+++ DaikichiRoutes/src/main/java/com/castillo/daikichiroutes/DaikichiRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.castillo.daikichiroutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiRoutesApplication.class, args);
+	}
+
+}

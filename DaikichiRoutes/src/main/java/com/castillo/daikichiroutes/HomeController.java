e09fab96d713e31cc1df326262279e927449e130
giannicastillo <66094112+giannicastillo@users.noreply.github.com>
2022-02-11 15:44:57 -0500

    Spring tool Suites Daikichi 1 and 2 complete!

--- DaikichiRoutes/src/main/java/com/castillo/daikichiroutes/HomeController.java
+++ DaikichiRoutes/src/main/java/com/castillo/daikichiroutes/HomeController.java
@@ -1,5 +1,6 @@
 package com.castillo.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,25 @@ public class HomeController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 		
-		
 	}
+	
+	@RequestMapping("/daikichi/travel/{city}")
+	public String city(@PathVariable("city") String city) {
+		System.out.println(city);
+		return String.format("Congratulations! You will soon travel to %s", city);
+	}
+	
+	@RequestMapping("/daikichi/lotto/{num}")
+	public String num(@PathVariable("num") int num) {	
+		//If you divide by evenly by 2 and get remainder then show 2nd statement
+		if(num%2 ==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}else {
+			
+		}
+		return String.format("You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.");
+	}
+	
+	
 
 }
